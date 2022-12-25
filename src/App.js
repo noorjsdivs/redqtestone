7fc6c9767e8cc38dba7d6cce6ae75025d52402f7
Noor Mohammad <noormohammad@AAOMAN.COM>
2022-12-25 10:56:17 +0400

    commmitted for the first time

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="A">
+        <h1>A</h1>
+      </div>
+      <div className="B">
+        <h1>B</h1>
+      </div>
+      <div className="C">
+        <h1>C</h1>
+      </div>
     </div>
   );
 }
